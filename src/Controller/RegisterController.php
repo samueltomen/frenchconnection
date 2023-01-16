ab242ca6489e52dc374c452a8bd84c8981fa7885
Dytsaw <tomennana.samuel@outlook.fr>
2023-01-16 19:53:11 +0100

    bug doctrine NOT FIXED

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -5,17 +5,33 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request): Response //permet de manipuler l'objet request pour savoir si le formulaire est correct
     {
 
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        //Injection de dependance
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $doctrine = $doctrine->getManager();
+            
+            $user = $form->getData();
+
+            // dd($user); // dd permet d'analyser ce qu'il y a dans la variable $user
+
+            $doctrine->persist($user);
+            $doctrine->flush();
+        }
 
         return $this->render('register/index.html.twig', [
             'form' => $form->createView()
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +18,40 @@ class RegisterType extends AbstractType
         array $options
     ): void {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email') // ceci est comme un input
-            ->add('password');
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Saisissez votre prénom',
+                ],
+            ])
+
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Saisissez votre nom',
+                ],
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre adresse mail',
+                ],
+            ]) // ceci est comme un input
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir un mot de passe',
+                ],
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Confirmez votre mot de passe',
+                'mapped'=> false, //indique a Symfony que la propriéte ne doit pas lier au formulaire 
+                'attr' => ['placeholder' => 'Confirmez votre mot de passe'],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire",
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
